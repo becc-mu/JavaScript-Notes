91f65e244328d38db95e8bb026674146639334d8
becc-mu <becc.mulugeta@gmail.com>
2019-02-25 16:57:50 +0000

    second test added

--- Just-Crash-Course/functions.test.js
+++ Just-Crash-Course/functions.test.js
@@ -3,3 +3,6 @@ const functions = require('./functions');
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+test('Adds 2 + 2 to NOT equal 3', () => {
+  expect(functions.add(2, 2)).not.toBe(3);
+});
