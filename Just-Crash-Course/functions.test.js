9f17d3343329295958c51f1c57ada430e20b7e37
becc-mu <becc.mulugeta@gmail.com>
2019-02-25 17:30:48 +0000

    falsy matches

--- Just-Crash-Course/functions.test.js
+++ Just-Crash-Course/functions.test.js
@@ -1,5 +1,10 @@
 const functions = require('./functions');
 
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
@@ -7,13 +12,14 @@ test('Adds 2 + 2 to NOT equal 3', () => {
   expect(functions.add(2, 2)).not.toBe(3);
 });
 
-// CHECK FOR TRUTHY & FALSY VALUES
-// toBeNull matches only null
-// toBeUndefined matches only undefined
-// toBeDefined is the opposite of toBeUndefined
-// toBeTruthy matches anything that an if statement treats as true
-// toBeFalsy matches anything that an if statement treats as false
+
 
 test('Should be null', () => {
   expect(functions.isNull()).toBeNull();
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeFalsy matches anything that an if statement treats as false
+test('Shoul be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
 });
